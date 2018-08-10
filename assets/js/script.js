13b7ccaff43c573aee6d59f4163dd159067779db
Jessika Costa Cardoso <jessika.cost@gmail.com>
2018-08-10 09:59:47 -0300

    update navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,27 +26,29 @@ $(document).ready(function () {
         exibirCarreira()
     })
 
+    const acessoLista = document.querySelector(".dropdown")
+    const acessoimgMenu = document.querySelector(".menu-hamburguer")
+    const acessoImgLogo = document.querySelector(".logo")
 
     const menu = document.querySelector(".menu-hamburguer")
     menu.addEventListener("click", function(){
         console.log("ok")
-        const acessoLista = document.querySelector(".dropdown")
-        const acessoimgMenu = document.querySelector(".menu-hamburguer")
-        const acessoImgLogo = document.querySelector(".logo")
         acessoLista.style.display="block"
         acessoImgLogo.style.display="none"
         acessoimgMenu.style.display="none"
     })
 
+    const fecharMenu = document.querySelector(".fechar")
+    fecharMenu.addEventListener("click", function(){
+        console.log("ok")
+        acessoLista.style.display="none"
+        acessoImgLogo.style.display="block"
+        acessoimgMenu.style.display="block"
+    })
 
 
-    // $(".menu-hamburguer").click(function(){
-    //     $(".dropdown").slideToggle();
-    // });
 
-    // $("a.dropdown-link").click(function(){
-    //     $(".dropdown").hide();
-    // });
+
 
 
 })
